9f66b2a71a0d7db79cf5c5cb6df8b3b3e449cb48
Gidrak <paskark@gmail.com>
2022-09-21 11:18:18 +0300

    Задали ввод эелементов массива

---
+++ FZ/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+string[] str = CreatArray("Введите количесвто элементов массива: ");
+Console.Write("Заданный массив с длиной элементов <= 3: ");
+
+
+string[] CreatArray(string massage)
+{
+    Console.Write(massage);
+    int Length = int.Parse(Console.ReadLine());
+    string[] array = new string[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = Console.ReadLine();
+
+    }
+    return array;
+}
